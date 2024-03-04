83df660b3c48e5b49ba462f6d0640a3bff421990
unknown <chirayair@gmail.com>
2024-03-03 20:12:36 -0400

    methods

--- src/02-methods.ts
+++ src/02-methods.ts
@@ -8,4 +8,24 @@ export class MyDate {
     this.month = month;
     this.day = day;
   }
+  printFormat(): string {
+    return `${this.day}/${this.month}/${this.year}`;
+  }
+  add(amount: number, type: 'days' | 'months' | 'years') {
+    if (type === 'days') {
+      this.day += amount;
+    }
+    if (type === 'months') {
+      this.month += amount;
+    }
+    if (type === 'years') {
+      this.year += amount;
+    }
+  }
 }
+
+const myDate = new MyDate(1998, 3, 7)
+console.log(myDate.printFormat())
+myDate.add(3, 'days')
+console.log(myDate.printFormat())
+
